2cd00609d3364f8ed91086aed4ca2a23929d8a2e
Oscar Carballal Prego <oscar@lareira.digital>
2023-07-18 14:00:57 +0200

    Removed build folder, updated CI, commented out langs

--- build/static/js/app.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".eat_that_cookie").forEach((closeButton) => {
-    closeButton.addEventListener("click", () =>
-        closeButton.closest(".banner").remove()
-    );
-});
